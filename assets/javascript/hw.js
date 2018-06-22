d75c351b86c3faba3c5bad26550d841d229dd648
Nicole Richardson <njrichardson95@gmail.com>
2018-06-21 21:29:13 -0400

    final changes and updates

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -36,13 +36,12 @@
            // Adding show from the textbox to our array
            shows.push(show);
 
-           // Calling renderButtons which handles the processing of our movie array
+           // Calling renderButtons which handles the processing of our show array
            renderButtons();
            newButton();
          });
 
-         // Adding a click event listener to all ements with a class of "movie-btn"
-         // $(document).on("click", ".show-btn", showDiv);
+
 
          // Calling the renderButtons function to display the intial buttons
          renderButtons();
@@ -82,7 +81,7 @@ function newButton() {
         showImage.attr("data-still", results[i].images.fixed_height_still.url);
         showImage.attr("data-animate", results[i].images.fixed_height.url);
         showImage.attr("data-state", "animate");
-        // Appending the paragraph and image tag to the foodDiv
+        // Appending the paragraph and image tag to the showDiv
         showDiv.append(p);
         showDiv.append(showImage);
         // Prependng the foodDiv to the HTML page in the "#foodGifs" div
@@ -107,68 +106,3 @@ function newButton() {
 }
 
 newButton();
-   //   $("button").on("click", function() {
-   //    // Grabbing and storing the data-animal property value from the button
-   //    var show = $(this).attr("data-name");
-   //
-   //    // Constructing a queryURL using the animal name
-   //    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-   //    show + "&api_key=dc6zaTOxFJmzC&limit=10";
-   //
-   //  $.ajax({
-   //    url: queryURL,
-   //    method: "GET"
-   //  })
-   //
-   //      // After data comes back from the request
-   //      .then(function(response) {
-   //        console.log(queryURL);
-   //
-   //        console.log(response);
-   //        // storing the data from the AJAX request in the results variable
-   //        var results = response.data;
-   //
-   //        // Looping through each result item
-   //        for (var i = 0; i < results.length; i++) {
-   //
-   //          // Creating and storing a div tag
-   //          var showDiv = $("<div>");
-   //          showDiv.addClass("newDiv");
-   //
-   //          // Creating a paragraph tag with the result item's rating
-   //          var p = $("<p>").text("Rating: " + results[i].rating);
-   //
-   //          // Creating and storing an image tag
-   //          var showImage = $("<img>");
-   //
-   //        showImage.attr("src", results[i].images.fixed_height.url);
-   //        showImage.attr("data-still", results[i].images.fixed_height_still.url);
-   //        showImage.attr("data-animate", results[i].images.fixed_height.url);
-   //        showImage.attr("data-state", "animate");
-   //          // Setting the src attribute of the image to a property pulled off the result item
-   //
-   //          // Appending the paragraph and image tag to the animalDiv
-   //          showDiv.append(p);
-   //          showDiv.append(showImage);
-   //
-   //          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-   //          $("#gifsappear").append(showDiv);
-   //        }
-   //      });
-   //  });
-   //
-   // //
-   // // $(".gif").on("click", function() {
-   // //    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-   // //    var state = $(this).attr("data-state");
-   // //    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-   // //    // Then, set the image's data-state to animate
-   // //    // Else set src to the data-still value
-   // //    if (state === "still") {
-   // //      $(this).attr("src", $(this).attr("data-animate"));
-   // //      $(this).attr("data-state", "animate");
-   // //    } else {
-   // //      $(this).attr("src", $(this).attr("data-still"));
-   // //      $(this).attr("data-state", "still");
-   // //    }
-   // //  });
